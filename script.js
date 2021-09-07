0e3cd281ca6ca923c592efdd6d64b1d19ba793fe
abdirahman <arahmanmoh776@gmail.com>
2021-09-07 05:27:32 -0700

    adding starting project

---
+++ script.js
@@ -0,0 +1,36 @@
+// var btnclick=document.getElementById("btnclick");
+// var txtuser=document.getElementById("usertext");
+// var ul=document.getElementsByTagName("ul")[0];
+
+// function firststring(){
+  
+// }
+   
+
+
+// function createNewElemnt(){
+//   var li=document.createElement("li");
+//   li.appendChild(document.createTextNode(txtuser.value))
+// ul.appendChild(li);
+// txtuser.value="";
+// }
+// btnclick.addEventListener("click",function(){
+//     createNewElemnt();
+// });
+
+// txtuser.addEventListener("keypress",function(){
+//   if(event.which==13){
+//      createNewElemnt(); 
+//   }
+    
+// })
+
+var color1=document.getElementById('txtcolor1');
+var color2=document.getElementById('txtcolor2');
+ 
+ 
+color1.addEventListener('input',function(){
+
+  
+})
+
